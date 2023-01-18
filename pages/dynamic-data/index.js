80134d57e3713fd8124cf5c85710d2561e9e798c
InspUsman <fawad@inspedium.com>
2023-01-18 14:24:19 +0500

    static props with revalidate added

--- pages/dynamic-data/index.js
+++ pages/dynamic-data/index.js
@@ -4,7 +4,7 @@ const DynamicData = ({ data }) => {
   if (data) return <div>{data.id}</div>;
 };
 
-export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
   const response = await fetch("https://random-data-api.com/api/v2/users");
   const responseData = await response.json();
 
@@ -12,6 +12,7 @@ export const getServerSideProps = async () => {
     props: {
       data: responseData,
     },
+    revalidate: 5,
   };
 };
 
