cfad9e5d640bde56077aac55b52f543635fef514
InspUsman <fawad@inspedium.com>
2023-01-18 12:52:19 +0500

    first commit

---
+++ pages/index.js
@@ -0,0 +1,45 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
+
+import utilStyles from "../styles/utils.module.css";
+
+export async function getStaticProps() {
+  // KEEP IN MIND getServerSideProps as well as useSWR
+  const allPostsData = await getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>I am Muhammad Usman. I am a big fan of Next JS xD</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, title, body }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id.toString()}`}>{title}</Link>
+              <br />
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
